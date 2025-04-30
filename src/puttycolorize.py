d4f5f95c3f9d716e5f213f76133dc146093a04e2
S.Hag <stefan_hag@mailbox.org>
2025-04-30 08:28:15 +0200

    work

--- src/puttycolorize.py
+++ src/puttycolorize.py
@@ -1,4 +1,5 @@
 import winreg
+import os
 
 from pathlib import Path
 
@@ -9,9 +10,15 @@ class Putty:
     regKey = r"Software\SimonTatham\PuTTY\Sessions"
     regFullKey = r"HKEY_CURRENT_USER" + "\\" + regKey
 
+    data = []
+
     def __init__(self):
-        self.rootDir = Path(__file__).parent
-        # self.includeFile = os.path.normpath(os.path.join(self.binPath, "include.txt"))
+        self.rootPath = Path(__file__).parent
+        self.regFile = os.path.normpath(os.path.join(self.rootPath, "PuttyBackup.reg"))
+
+    def printNStore(self, msg):
+        print(msg.strip())
+        self.data.append(msg)
 
     def change_putty_setting(session_name, setting_name, new_value):
         # Base path for PuTTY session
@@ -32,7 +39,8 @@ class Putty:
         except WindowsError as e:
             print(f"Error: {str(e)}")
 
-    def get_immediate_subkeys(self, hive, key_path):
+    def get_subkeys(self, hive, key_path):
+        """get all Subkeys from a Key"""
         try:
             # Open the registry key
             key = winreg.OpenKey(hive, key_path, 0, winreg.KEY_READ)
@@ -58,6 +66,11 @@ class Putty:
             print(f"Error accessing {key_path}: {str(e)}")
             return []
 
+    def getType(self, type):
+        if type == 4:
+            return "dword:"
+        return ""
+
     def get_all_keys_and_subkeys(self, hive, reg_path):
         """
         Recursively get all keys and their subkeys from a specified registry path
@@ -68,9 +81,8 @@ class Putty:
         try:
             # Open the registry key
             registry_key = winreg.OpenKey(hive, reg_path, 0, winreg.KEY_READ)
-
             # Print current key path
-            print(f"\nKey: {reg_path}")
+            self.printNStore(f"\n[HKEY_CURRENT_USER\\{reg_path}]\n")
 
             try:
                 # Get values in current key
@@ -78,53 +90,47 @@ class Putty:
                 while True:
                     try:
                         name, value, type = winreg.EnumValue(registry_key, i)
-                        print(f"Value {i + 1}:")
-                        print(f"  Name: {name}")
-                        print(f"  Data: {value}")
-                        print(f"  Type: {type}")
+                        if self.getType(type) == "":
+                            self.printNStore(f'"{name}"="{value}"\n')
+                        else:
+                            self.printNStore(f'"{name}"={self.getType(type)}{value}\n')
                         i += 1
                     except WindowsError as err:
-                        print(err)
+                        # print(err)
                         break
             except WindowsError:
                 pass
-
-            try:
-                # Get subkeys
-                i = 0
-                while True:
-                    try:
-                        subkey_name = winreg.EnumKey(registry_key, i)
-                        subkey_path = f"{reg_path}\\{subkey_name}"
-                        # Recursive call for subkeys
-                        get_all_keys_and_subkeys(hive, subkey_path)
-                        i += 1
-                    except WindowsError:
-                        break
-            except WindowsError:
-                pass
-
-            winreg.CloseKey(registry_key)
 
         except WindowsError as e:
             print(f"Error accessing {reg_path}: {str(e)}")
+
+    def savetoFile(self):
+        """save putty export to *.reg File"""
+        with open(self.regFile, "w") as f:
+            for d in self.data:
+                f.write(d)
+        f.close()
 
     def exportPutty(self):
         print(f"*** Reading from {self.regFullKey} ***")
         print("=" * 50)
 
-        subkeys = self.get_immediate_subkeys(self.hive, self.regKey)
+        msg = f"\n[{self.regFullKey}]\n"
+        self.data.append(msg)
+
+        subkeys = self.get_subkeys(self.hive, self.regKey)
         for key in subkeys:
             newkey = self.regKey + "\\" + key
-
             self.get_all_keys_and_subkeys(self.hive, newkey)
 
-        # Example usage
+        self.savetoFile()
+
+        # ???Example usage ---------------------------------------------
         session_name = "Default%20Settings"  # Use the exact name as in registry
         setting_name = "Colour0"
         new_value = "131,148,150"
 
-        change_putty_setting(session_name, setting_name, new_value)
+        # change_putty_setting(session_name, setting_name, new_value)
 
     def start(self):
         self.exportPutty()
